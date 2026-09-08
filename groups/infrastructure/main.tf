871e42622471faf25099b8ab96315a83846a1799
sgopi108 <sgopi@companieshouse.gov.uk>
2026-09-08 15:52:29 +0100

    feat: update code for-pipeline-recommission

--- groups/infrastructure/main.tf
+++ groups/infrastructure/main.tf
@@ -1,13 +1,13 @@
 terraform {
-  required_version = "~> 1.3"
+  required_version = ">= 1.3, < 2.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.54.0"
+      version = ">= 5.0, < 7.0"
     }
     vault = {
       source  = "hashicorp/vault"
-      version = "~> 3.18.0"
+      version = ">= 5.0, < 6.0"
     }
   }
 }
@@ -20,33 +20,49 @@ terraform {
   backend "s3" {}
 }
 
-module "ecs-cluster" {
-  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/ecs-cluster?ref=1.0.231"
-
-  stack_name                  = local.stack_name
-  name_prefix                 = local.name_prefix
-  environment                 = var.environment
-  aws_profile                 = var.aws_profile
-  vpc_id                      = data.aws_vpc.vpc.id
-  subnet_ids                  = local.application_subnet_ids
-  ec2_key_pair_name           = var.ec2_key_pair_name
-  ec2_instance_type           = var.ec2_instance_type
-  ec2_image_id                = var.ec2_image_id
-  asg_max_instance_count      = var.asg_max_instance_count
-  asg_min_instance_count      = var.asg_min_instance_count
-  enable_container_insights   = var.enable_container_insights
-  asg_desired_instance_count  = var.asg_desired_instance_count
-  scaledown_schedule          = var.asg_scaledown_schedule
-  scaleup_schedule            = var.asg_scaleup_schedule
-  enable_asg_autoscaling      = var.enable_asg_autoscaling
-  notify_topic_slack_endpoint = local.notify_topic_slack_endpoint
-}
-
-module "secrets" {
-  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/secrets?ref=1.0.231"
+module "ecs_cluster" {
+  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/ecs-cluster?ref=1.0.412"
 
   environment = var.environment
   name_prefix = local.name_prefix
-  secrets     = local.parameter_store_secrets
-  kms_key_id  = data.aws_kms_key.stack_configs.id
+  stack_name  = local.stack_name
+  subnet_ids  = local.application_subnet_ids
+  vpc_id      = data.aws_vpc.vpc.id
+
+  ec2_key_pair_name = var.ec2_key_pair_name
+  ec2_instance_type = var.ec2_instance_type
+  ec2_image_id      = var.ec2_image_id
+
+  asg_max_instance_count     = var.asg_max_instance_count
+  asg_min_instance_count     = var.asg_min_instance_count
+  asg_desired_instance_count = var.asg_desired_instance_count
+
+  scaledown_schedule     = var.asg_scaledown_schedule
+  scaleup_schedule       = var.asg_scaleup_schedule
+
+  enable_container_insights   = var.enable_container_insights
+  notify_topic_slack_endpoint = local.notify_topic_slack_endpoint
+
+  default_tags = merge(
+    module.iac_tags.tags,
+    module.owner_tags.tags,
+  )
+}
+
+moved {
+  from = module.ecs-cluster
+  to   = module.ecs_cluster
+}
+
+module "iac_tags" {
+  source = "git@github.com:companieshouse/terraform-modules//aws/tagging/iac?ref=tags/1.0.412"
+
+  group           = "infrastructure"
+  source_code_url = "https://github.com/companieshouse/identity-service-stack"
+}
+
+module "owner_tags" {
+  source = "git@github.com:companieshouse/terraform-modules//aws/tagging/owner?ref=tags/1.0.412"
+
+  platform_owner    = "platform"
 }
--- groups/infrastructure/variables.tf
+++ groups/infrastructure/variables.tf
@@ -3,11 +3,13 @@ variable "environment" {
   type        = string
   description = "The environment name, defined in envrionments vars."
 }
+
 variable "aws_region" {
   default     = "eu-west-2"
   type        = string
   description = "The AWS region for deployment."
 }
+
 variable "aws_profile" {
   default     = "development-eu-west-2"
   type        = string
@@ -19,13 +21,15 @@ variable "ec2_key_pair_name" {
   type        = string
   description = "The key pair for SSH access to ec2 instances in the clusters."
 }
+
 variable "ec2_instance_type" {
   default     = "t3.medium"
   type        = string
   description = "The instance type for ec2 instances in the clusters."
 }
+
 variable "ec2_image_id" {
-  default     = "ami-04018f95156d810bc" # ECS optimized Amazon2 Linux in London created 15/03/2023
+  default     = null
   type        = string
   description = "The machine image name for the ECS cluster launch configuration."
 }
@@ -54,11 +58,13 @@ variable "enable_asg_autoscaling" {
   type        = bool
   description = "Whether to enable auto-scaling of the ASG by creating a capacity provider for the ECS cluster."
 }
+
 variable "asg_min_instance_count" {
   default     = 0
   type        = number
   description = "The minimum allowed number of instances in the autoscaling group for the cluster."
 }
+
 variable "asg_desired_instance_count" {
   default     = 0
   type        = number
@@ -70,4 +76,10 @@ variable "enable_container_insights" {
   type        = bool
   description = "A boolean value indicating whether to enable Container Insights or not"
   default     = true
+}
+
+variable "enable_asg_autoscaling" {
+  default     = true
+  type        = bool
+  description = "Whether to enable auto-scaling of the ASG by creating a capacity provider for the ECS cluster."
 }
